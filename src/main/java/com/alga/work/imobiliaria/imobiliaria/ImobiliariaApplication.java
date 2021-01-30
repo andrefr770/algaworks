018db6f50968b79b2cac00704ef4b35b8389762b
André Ferreira <andre.ferreira@decode.pt>
2021-01-30 17:52:11 +0000

    Criação da tabela registarCasa na base de dados e cri

--- src/main/java/com/alga/work/imobiliaria/imobiliaria/ImobiliariaApplication.java
+++ src/main/java/com/alga/work/imobiliaria/imobiliaria/ImobiliariaApplication.java
@@ -3,6 +3,7 @@ package com.alga.work.imobiliaria.imobiliaria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Repository;
 
 @SpringBootApplication
 @Repository(value="com.alga.work.imobiliaria.imobiliaria")
+@EntityScan(basePackages = {"com.alga.work.imobiliaria.imobiliaria.model"})
 public class ImobiliariaApplication {
 
 	public static void main(String[] args) {
